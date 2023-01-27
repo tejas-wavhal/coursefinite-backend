88076fa9e929064ebcaf2b904018aaf5bab63a2f
Tejas <twavhal7514@gmail.com>
2023-01-27 10:58:14 +0530

    initial

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import mongoose, { connect } from "mongoose";
 
 export const connectDB = async () => {
-  const { connection } = await mongoose.set('strictQuery', true).connect(process.env.MONGO_URI)
+  const { connection } = await mongoose.set('strictQuery', true).connect(process.env.MONGO_URI,{ useNewUrlParser: true })
   console.log(`🟢MongoDB connected with ${connection.host}`)
 }
